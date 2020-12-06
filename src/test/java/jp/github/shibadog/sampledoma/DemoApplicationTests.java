574eb63af223418c9894dbfea8cf7b29e0439117
shibadog <shibadog99@gmail.com>
2020-12-06 23:23:52 +0900

    init

---
+++ src/test/java/jp/github/shibadog/sampledoma/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.github.shibadog.sampledoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
